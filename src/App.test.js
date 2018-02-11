bbc188665caf2ab3792d63304e70dba4807b7d97
Craig Meinschein <cmeinsch@gmail.com>
2018-02-11 14:12:09 -0800

    Fix prop warnings in test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,10 @@ import { App } from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App initiativeList={[]} />, div)
+  ReactDOM.render(<App
+      initiativeList={[]}
+      addInitiative={() => {}}
+      nextTurn={() => {}} />,
+    div)
   ReactDOM.unmountComponentAtNode(div)
 })

539be31c28d8b24d2c6ee98bc08ab113d3798c0c
Craig Meinschein <cmeinsch@gmail.com>
2018-02-16 19:46:17 -0800

    Fix App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(
-    <App
-      initiativeList={[]}
-      addInitiative={() => {}}
-      nextTurn={() => {}} />,
+    <Provider store={createStore(() => {})}>
+      <App
+        initiativeList={[]}
+        addInitiative={() => {}}
+        nextTurn={() => {}} />
+    </Provider>,
     div)
   ReactDOM.unmountComponentAtNode(div)
 })
